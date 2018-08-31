2465623f6113480ed45caf7e1960a10c0133f0e3
firiz <farshad.sahafzadeh@tap30co.com>
2018-09-01 01:06:42 +0430

    Change server to be loaded on random port

--- service/echo/index.js
+++ service/echo/index.js
@@ -1,7 +1,9 @@
 const PROTO_PATH = `${__dirname}/../../proto/service/echo.proto`;
+const HOST = '0.0.0.0';
 
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
+const getPort = require('get-port');
 
 const packageDefinition = protoLoader.loadSync(
   PROTO_PATH,
@@ -27,6 +29,15 @@ server.addService(echoProto.EchoService.service, {
   },
 });
 
-server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-console.log('Server running at http://0.0.0.0:50051');
-server.start();
+const run = async () => {
+  try {
+    const port = await getPort();
+    server.bind(`${HOST}:${port}`, grpc.ServerCredentials.createInsecure());
+    console.log(`Server running at http://${HOST}:${port}`);
+    server.start();
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+};
+
+run();
