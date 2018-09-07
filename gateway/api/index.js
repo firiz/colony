975fcd527a3960540d5f0f20461dc8fddf3aa555
firiz <f.sahafzadeh@gmail.com>
2018-09-07 17:59:51 +0430

    Add colony-rpc library and use in logic

--- gateway/api/index.js
+++ gateway/api/index.js
@@ -1,39 +1,20 @@
-const amqp = require('amqplib');
+const RabbitRpc = require('colony-rpc/rabbit');
 
-const args = process.argv.slice(2);
-
-function generateUuid() {
-  return Math.random().toString()
-    + Math.random().toString()
-    + Math.random().toString();
-}
-
-if (args.length === 0) {
-  console.log('Usage: rpc_client.js num');
-  process.exit(1);
-}
+const client = new RabbitRpc();
 
 const run = async () => {
   try {
-    const conn = await amqp.connect('amqp://localhost');
-    const ch = await conn.createChannel();
-    const q = await ch.assertQueue('', { exclusive: true });
+    await client.init();
 
-    const corr = generateUuid();
-    const num = parseInt(args[0], 10);
+    while (true) {
+      const num = Math.round(Math.random() * 30);
 
-    console.log(' [x] Requesting fib(%d)', num);
+      console.log(' [x] Requesting fib(%d)', num);
 
-    ch.consume(q.queue, (msg) => {
-      if (msg.properties.correlationId === corr) {
-        console.log(' [.] Got %s', msg.content.toString());
-        setTimeout(() => { conn.close(); process.exit(0); }, 500);
-      }
-    }, { noAck: true });
+      const result = await client.call('fibonacci.calculate', num);
 
-    ch.sendToQueue('rpc_queue',
-      Buffer.from(num.toString()),
-      { correlationId: corr, replyTo: q.queue });
+      console.log(' [.] Got %s', result.toString());
+    }
   } catch (error) {
     console.log('Error: ', error);
   }
---
+++ service/fibonacci/index.js
@@ -0,0 +1,31 @@
+const RabbitRpc = require('colony-rpc/rabbit');
+
+const client = new RabbitRpc();
+
+function fibonacci(n) {
+  if (n === 0 || n === 1) {
+    return n;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+const run = async () => {
+  try {
+    await client.init();
+
+    client.addHandler('fibonacci.calculate', (data) => {
+      const n = Math.min(30, parseInt(data.toString(), 10));
+
+      console.log(' [.] fib(%d)', n);
+
+      const r = fibonacci(n);
+
+      return r;
+    });
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+};
+
+run();
