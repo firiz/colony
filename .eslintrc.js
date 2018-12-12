e1f0d080efa50aa1e918d79876fe6bcdadc88365
firiz <f.sahafzadeh@gmail.com>
2018-12-12 23:12:26 +0330

    Eslint support

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
     'no-param-reassign': 0,
     'no-trailing-spaces': 0,
     'no-underscore-dangle': 0,
+    'no-await-in-loop': 0,
+    'no-restricted-syntax': 0,
   },
   env: {
     'jest/globals': true,
